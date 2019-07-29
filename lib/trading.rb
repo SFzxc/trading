4e0b50bf9d8e55e95cb0391006b379e3ec765540
SFzxc <hoangphuocvalong@gmail.com>
2019-07-29 11:03:16 +0700

    Rename command

--- lib/trading.rb
+++ lib/trading.rb
@@ -1,6 +1,21 @@
 require "trading/version"
+require 'gli'
+
+include GLI::App
 
 module Trading
-  class Error < StandardError; end
-  # Your code goes here...
+  program_desc 'Support Trading and check coin marketrate via terminal'
+
+  version Trading::VERSION
+
+  desc 'Get prices for all currency pairs on popular exchanges'
+  command :price do |c|
+    c.switch :d
+    c.action do |global_options, options, args|
+      puts "#{args[0]}"
+      puts "#{args[1]}"
+    end
+  end
+
+  exit run(ARGV)
 end
